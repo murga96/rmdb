7e76e14beaedc349db5d0db800d3757cd8fa7bb8
Gustavo Martínez de Murga <gustavo.murga1996@gmail.com>
2022-08-04 19:03:01 -0400

    Creating the header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./GlobalStyle";
 
-const App = () => <div>Start here!</div>
+const App = () => (
+  <div className="App">
+    <GlobalStyle />
+    <Header />
+  </div>
+);
 
 export default App;
